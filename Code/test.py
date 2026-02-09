d0c11338666b1a8cfb0acc4935ad252dd8f2aa0e
Brician <belizairebrician@gmail.com>
2026-02-09 10:53:59 -0500

    Starting structure

---
+++ Code/test.py
@@ -0,0 +1,18 @@
+import unittest
+import university
+
+class TestUniversity(unittest.TestCase):
+
+    """Tests the university class for object creation, adding courses,
+       duplicate course management, adding a student, duplicate student
+       management, get student info, get nonexistent student info,
+       get course, get nonexistent course"""
+    
+    def test_make_object(self):
+        
+        """Make a University object and see iff it exists"""
+
+        UCONN = University()
+
+
+    
